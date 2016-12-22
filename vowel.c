c66d936136ebf9ce9cb03281966c333128dcf4f8
surekhad96 <surekhasugan96@gmail.com>
2016-12-22 14:36:25 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char d;
+scanf("%c",&d)
+if(d=(A==a||E==e||I==i||O==o||U==u))
+{
+printf("%d is a vowel\n");
+}
+else
+{
+printf("%d is a consonant\n");
+}
+return 0;
+}
